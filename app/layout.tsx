e6ad9cd3b72ff51d1ec60fbf47b089dec1274267
Priyak Dey <priyak1991dey@gmail.com>
2025-10-21 00:51:41 +0530

    add speed insights from vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "next-themes";
@@ -44,6 +45,7 @@ export default function RootLayout({
             <Footer />
           </div>
         </ThemeProvider>
+        <SpeedInsights />
         <Analytics />
       </body>
     </html>
